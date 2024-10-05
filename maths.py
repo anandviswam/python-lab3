8f8302c8af4e6d678489d9a4148afde3476dc6cd
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-05 16:33:13 +0530

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,8 @@
+import math
+num=int(input("enter your number:"))
+sq_root=math.sqrt(num)
+factorial=math.factorial(num)
+raised_pow=math.pow(num,2)
+print("square root of 5:",sq_root)
+print("factorial of 5:",factorial)
+print("5 raised to power 2:",raised_pow)
---
+++ twinkle.py
@@ -0,0 +1,2 @@
+string=("twinkle,twinkle,little star,\n\t How I wonder what you are! \n\t\t up above the world so high, \n\t\t like a diamond in the sky.\n Twinkle,twinkle little star,\n\t how i wonder what you are")
+print(string)
